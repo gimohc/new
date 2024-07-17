7c2bda2e15f43f741ec9db7f836fa3f8dd9e2f7b
gimohc <mrlenovog50@gmail.com>
2024-07-17 20:45:02 +0300

    customer database configured

--- Java Spring/springinit/src/main/java/com/momo/springinit/Customer.java
+++ Java Spring/springinit/src/main/java/com/momo/springinit/Customer.java
@@ -1,8 +1,21 @@
 package com.momo.springinit;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private int id;
     private String name;
     private String email;
