128f3857134ddded678b448fc03332d30df7da38
gimohc <mrlenovog50@gmail.com>
2024-07-29 18:22:50 +0300

    final project done ready for discussion

--- Java Spring/springinit/src/main/java/com/momo/springinit/Main.java
+++ Java Spring/springinit/src/main/java/com/momo/springinit/Main.java
@@ -2,23 +2,48 @@ package com.momo.springinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
 
+	private final CustomerRepo customerRepo;
+	public Main(CustomerRepo customerRepo) {
+		this.customerRepo = customerRepo;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
-	/*@GetMapping("/greet")
-	public GreetResponse greet() {
-		return new GreetResponse("Hello",List.of("a", "b", "c"));
-	}
-	record GreetResponse(String greet, List<String> food) {
 
-	}*/
+	@GetMapping
+	public List<Customer> getCustomers() {
+		return customerRepo.findAll();
+	}
+
+	record NewCustomerRequest (
+		String name,
+		String email,
+		Integer age
+	){}
+
+	@PostMapping
+	public void addCustomer(@RequestBody NewCustomerRequest request) {
+		Customer customer = new Customer();
+		customer.setName(request.name);
+		customer.setEmail(request.email);
+		customer.setAge(request.age);
+		customerRepo.save(customer);
+
+	}
+
+	@DeleteMapping({"customerId"})
+	public void deleteCustomer(@PathVariable("customerId") Integer id) {
+		customerRepo.deleteById(id);
+	}
 }
+
+
